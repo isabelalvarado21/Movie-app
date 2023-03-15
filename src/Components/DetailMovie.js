03c6d11c0b7d2b5303f28e576ffccdc636ccdd31
alvaradomariaisabel21@gmail.com <alvaradomariaisabel21@gmail.com>
2023-03-15 19:09:57 +0100

    Agrego el link del trailer y los generos

--- src/Components/DetailMovie.js
+++ src/Components/DetailMovie.js
@@ -5,6 +5,7 @@ export const DetailMovie = () => {
 
     const params = useParams()
     const [movie, setMovie] = useState({})
+    const [trailer, setTrailer] = useState([])
     
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=7c1a3b7f576f57154e113773e6308ceb`)
@@ -12,15 +13,23 @@ export const DetailMovie = () => {
         .then(data => setMovie(data))
     }, [params.id])
 
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=7c1a3b7f576f57154e113773e6308ceb`)
+        .then(res => res.json())
+        .then(data => setTrailer(data.results[0]))
+    }, [params.id])
+
     return(
         
         <div>
-            <h1>Detalle</h1>
-            <h2>Soy la Pelicula: {movie.title}</h2>
-            {/* Falta poner los datos de Trailer y los generos de la pelicula */}
+           
+            <h1>{movie.title}</h1>
             <p>Descripcion: {movie.overview}</p>
-            {/* <p>soy el id: {movie.id}</p> */}
+            {movie?.genres?.map((genre) => (
+                      <p>{genre.name}</p>
+                    ))}
             <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="poster" />
+            <button><a href={`https://www.youtube.com/watch?v=${trailer.key}`}>trailer</a></button>
         </div>
     )
 }
