da7b9ece75870a65c39013fce9321d1980e7a1ef
july8712 <julietabondi@gmail.com>
2023-03-26 21:57:24 -0300

    Funcionamiento de menú hamburguesa

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,6 +9,7 @@ export const Navbar = () => {
 
     const navigate = useNavigate()
     const [valueInput, setValueInput] = useState("")
+    let [abierto, setAbierto] = useState(false)
     
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -17,14 +18,27 @@ export const Navbar = () => {
     const handleChange = (e) => {
        setValueInput (e.target.value)
     }
+
+    const mostrarMenu = () => {
+        if(abierto){
+            setAbierto(false)
+            console.log(abierto);
+        }else{
+            setAbierto(true)
+            console.log(abierto);
+        }
+    }
    
     return(
 
         <Box w='100%' p={4} bg='#030015' id='navbar'>
-            <Link to="/">
-                <Image src='assets/img/logo-movie.png' alt='Dan Abramov' w='150px' />
-            </Link>
-            <Stack direction='row' spacing={4} align='center' >
+            <Box display="flex" alignItems="center" justifyContent="space-between" id='iconosMenu'>
+                <Link to="/">
+                    <Image src='assets/img/logo-movie.png' alt='Dan Abramov' w='150px' />
+                </Link>
+                <Image src='assets/img/hamburger.svg' alt='Dan Abramov' w='35px' id='hamburguesa' onClick={mostrarMenu}/>
+            </Box>
+            <Box className={abierto ? 'abierto' : 'cerrado'} alignItems="center" direction='row' gap='3' align='center' id='containerButtons'>
                 <Link to="/">
                     <Button variant='ghost' color='#a9a3d3' _hover={{background: "#52002c", color: "white"}}>
                         Home
@@ -42,7 +56,7 @@ export const Navbar = () => {
                     <Input onChange={handleChange} width='auto' variant='filled' placeholder='ej. Batman' />
                     <Input type='submit' onChange={handleChange} width='auto' color='#a9a3d3' fontWeight='bold' ms='4' _hover={{background: "#ffffff", color: "#030015", fontWeight: 'bold'}}/>
                 </form>
-            </Stack>
+            </Box>
         </Box>
     )
 }
