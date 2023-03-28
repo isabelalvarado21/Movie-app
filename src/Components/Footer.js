f5339efabb5c20903f6a2dfa4236c556571e2034
july8712 <julietabondi@gmail.com>
2023-03-27 22:52:55 -0300

    arreglos estéticos en la Home

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import { Container, VStack,IconButton, Text, Link } from '@chakra-ui/react'
+import { Container, HStack,IconButton, Text, Link } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 export const Footer = () => {
@@ -7,17 +7,14 @@ export const Footer = () => {
     
     return(
         <>
-        <VStack>
-            <Container bg='#030015' w='100%' p={12} centerContent >
-                <Text color='#a9a3d3' >2023. Movie App.</Text>
-                <Text color='#a9a3d3' p='3'>Hecho por Julieta e Isabel.</Text>
-            <Link href={link}>
-                <IconButton 
-                    _hover={{background: "#52002c", color: "white"}}
-                    icon={<ExternalLinkIcon  />}/>
-            </Link>                   
+            <Container maxW="xl" bg='#030015' w='100%' p={12} centerContent >
+                <HStack>
+                    <Text color='#a9a3d3' >© Movie App - 2023 - Hecho con ❤ por Julieta e Isabel</Text>
+                    <Link href={link}>
+                        <IconButton ms='5' _hover={{background: "#52002c", color: "white"}} icon={<ExternalLinkIcon  />}/>
+                    </Link>                   
+                </HStack>
             </Container>
-        </VStack>
         </>
     )
 }
